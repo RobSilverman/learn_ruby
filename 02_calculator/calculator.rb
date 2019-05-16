cc9c97d124c3925e4027ffd18ec3b3e199e712ce
Rob Silverman <robertsilverman.425@gmail.com>
2019-05-16 18:47:48 -0400

    Solved
    solved 02_calculator (not incl. advanced)

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
 #write your code here
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def subtract (num3, num4)
+    num3 - num4
+end
+
+def sum arry
+    tot = 0
+    arry.each do |i|
+        tot += i
+    end
+    tot
+end
