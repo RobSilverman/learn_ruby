df0e85f3d93f8e3518206fbfd2526f44327d7acc
Rob Silverman <robertsilverman.425@gmail.com>
2019-05-21 18:17:13 -0400

    Solved
    solve 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+    
+    attr_reader :title
+    
+    def title=(str)
+        words = str.split(" ")
+        words.each do |i|
+            if i = %w{a an and the in of}
+                next
+            end
+            i.capitalize!
+        end
+        @title = words.join(" ")
+    end
+
 end
